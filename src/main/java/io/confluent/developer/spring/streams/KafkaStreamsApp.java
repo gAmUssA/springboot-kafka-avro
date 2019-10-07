2041cf8deecac018c965758246e88db9410f9ba2
Viktor Gamov <viktor@confluent.io>
2019-10-07 10:02:14 -0400

    Added Spring Cloud Streams ❤️ Kafka Streams example

---
+++ src/main/java/io/confluent/developer/spring/streams/KafkaStreamsApp.java
@@ -0,0 +1,70 @@
+package io.confluent.developer.spring.streams;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
+
+import io.confluent.developer.User;
+
+import static io.confluent.developer.spring.streams.Bindings.USERS;
+import static io.confluent.developer.spring.streams.Bindings.USERS_FILTERED;
+
+
+@EnableBinding(Bindings.class)
+@SpringBootApplication
+public class KafkaStreamsApp {
+
+  @Value("${topic.name}-filtered")
+  private String topicName;
+
+  @Value("${topic.partitions-num}")
+  private Integer partitions;
+
+  @Value("${topic.replication-factor}")
+  private short replicationFactor;
+
+
+  public static void main(String[] args) {
+    final SpringApplication application = new SpringApplication(KafkaStreamsApp.class);
+    // we don't need web/rest interface in this app 
+    application.setWebApplicationType(WebApplicationType.NONE);
+    application.run(args);
+  }
+
+  @Bean
+  NewTopic filteredTopic() {
+    return new NewTopic(topicName, partitions, replicationFactor);
+  }
+}
+
+interface Bindings {
+
+  String USERS_FILTERED = "users-filtered";
+  String USERS = "users";
+
+  @Input(USERS)
+  KStream<String, User> usersI();
+
+  @Output(USERS_FILTERED)
+  KStream<String, User> filteredUsers();
+}
+
+@Component
+class UserProcessor {
+
+  @StreamListener
+  @SendTo(USERS_FILTERED)
+  KStream<String, User> processUsers(@Input(USERS) KStream<String, User> inputStream) {
+    return inputStream.filter((s, user) -> user.getAge() < 40);
+  }
+}
